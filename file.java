4c7333790aed2ab5e41f57fb1fea4b2ce300c10d
Chandana1223 <chandana.k232@gmail.com>
2020-12-26 20:32:20 +0530

    file.java

---
+++ file.java
@@ -0,0 +1,14 @@
+class file
+{
+ public static void main(String[] args)
+   {
+    for (int i=1; i<=5; i++)
+      {
+        for (int s=1; s<=i; s++)
+          {
+         System.out.print(s);
+          }
+         System.out.println();
+      }
+   }
+}
